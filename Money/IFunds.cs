438e3a1dfa781da48879ca3c673d60f5daa5c4a2
Dex <77392785+Madared@users.noreply.github.com>
2024-03-30 00:55:02 +0000

    Added FundsGenerator

--- Money/IFunds.cs
+++ Money/IFunds.cs
@@ -1,8 +1,14 @@
 namespace Money;
 
-public interface IFunds
-{
+public interface IFunds {
     decimal Amount { get; }
     Currency.Currency Currency { get; }
+}
 
+public static class FundsGenerator {
+    public static IFunds Create(decimal amount, Currency.Currency currency) => amount switch {
+        < 0 => new Debt(NegativeDecimal.Create(amount).Data, currency),
+        > 0 => new Money(PositiveDecimal.Create(amount).Data, currency),
+        _ => new NoMoney(currency)
+    };
 }
---
+++ Money/NegativeDecimalMath.cs
@@ -0,0 +1,5 @@
+namespace Money;
+
+public class NegativeDecimalMath {
+    
+}
