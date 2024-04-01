1998f19f76ceaab146ce5e0c3b3d8e85079c02b2
Dex <77392785+Madared@users.noreply.github.com>
2024-04-01 18:08:39 +0100

    Fixed styling

--- Money/DecimalMath/DefaultMoneyMath.cs
+++ Money/DecimalMath/DefaultMoneyMath.cs
@@ -7,19 +7,22 @@ namespace Money.DecimalMath;
 
 public class DefaultMoneyMath {
     private readonly IMoneyCurrencyConverter _converter;
+
     public DefaultMoneyMath(IMoneyCurrencyConverter converter) {
         _converter = converter;
     }
+
     public Task<Result<Money>> Plus(Money first, Money second) => Apply(first, second, SameCurrencyPlus);
     public Task<Result<Money>> Times(Money first, Money second) => Apply(first, second, SameCurrencyTimes);
     public Task<Result<Money>> Divide(Money first, Money second) => Apply(first, second, SameCurrencyDivide);
     public Task<Result<Money>> Minus(Money first, Money second) => Apply(first, second, SameCurrencyMinus);
+
     private delegate Result<Money> MoneyMathOperation(Money first, Money second);
-    
-        private async Task<Result<Money>> Apply(Money first, Money second, MoneyMathOperation operation) => first.Currency == second.Currency
-            ? operation(first, second)
-            : await _converter.Convert(second, first.Currency)
-                .MapAsync(converted => operation(first, converted));
+
+    private async Task<Result<Money>> Apply(Money first, Money second, MoneyMathOperation operation) => first.Currency == second.Currency
+        ? operation(first, second)
+        : await _converter.Convert(second, first.Currency)
+            .MapAsync(converted => operation(first, converted));
 
     private Result<Money> SameCurrencyPlus(Money first, Money second) => first.CashAmount
         .Plus(second.CashAmount)
