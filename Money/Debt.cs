2d616794ad12d6a9a794fa38064170e8204e32e7
Dex <77392785+Madared@users.noreply.github.com>
2024-04-01 17:42:09 +0100

    Added static create methods to get specialized errors instead of default specified decimal errors

--- Money/Debt.cs
+++ Money/Debt.cs
@@ -1,9 +1,17 @@
 using Money.Currencies;
 using Money.Decimals;
+using Results;
 
 namespace Money;
 
 public record Debt(INegativeDecimal DebtAmount, Currency Currency) : INonPositiveFunds
 {
     decimal IFunds.Amount => DebtAmount.Amount;
+
+    public static Result<Debt> Create(decimal amount, Currency currency) {
+        Result<NegativeDecimal> negative = NegativeDecimal.Create(amount);
+        return negative.Failed
+            ? Result<Debt>.Fail(new InvalidDebtAmountError(amount))
+            : Result<Debt>.Ok(new Debt(negative.Data, currency));
+    }
 }
--- Money/Money.cs
+++ Money/Money.cs
@@ -7,30 +7,10 @@ namespace Money;
 public record Money(IPositiveDecimal CashAmount, Currency Currency) : INonNegativeFunds {
     decimal IFunds.Amount => CashAmount.Amount;
 
-    public static IFunds Create(INonNegativeDecimal nonNegativeDecimal, Currency currency) {
-        if (nonNegativeDecimal.IsZero()) return new NoMoney(currency);
-        return PositiveDecimal
-            .Create(nonNegativeDecimal.Amount)
-            .Map(amount => new Money(amount, currency))
-            .Data;
+    public static Result<Money> Create(decimal amount, Currency currency) {
+        Result<PositiveDecimal> positive = PositiveDecimal.Create(amount);
+        return positive.Failed 
+            ? Result<Money>.Fail(new InvalidMoneyAmountError(amount)) 
+            : new Money(positive.Data, currency).ToResult(new UnknownError());
     }
-}
-
-public interface INonNegativeDecimal {
-    decimal Amount { get; }
-    bool IsZero();
-}
-
-public record NonNegativeDecimal : INonNegativeDecimal {
-    public decimal Amount { get; }
-    public bool IsZero() => Amount == decimal.Zero;
-
-    private NonNegativeDecimal(decimal amount) {
-        if (amount < 0) throw new InvalidDataException();
-        Amount = amount;
-    }
-
-    public static Result<NonNegativeDecimal> Create(decimal amount) => amount < 0
-        ? Result<NonNegativeDecimal>.Fail(new UnknownError())
-        : Result<NonNegativeDecimal>.Ok(new NonNegativeDecimal(amount));
 }
