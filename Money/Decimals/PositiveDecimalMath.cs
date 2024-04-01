42f97e40d7010c9eebeaa52c8a9cd512845c0f19
Dex <77392785+Madared@users.noreply.github.com>
2024-04-01 22:24:50 +0100

    Added Throwing methods

--- Money/Decimals/PositiveDecimalMath.cs
+++ Money/Decimals/PositiveDecimalMath.cs
@@ -5,8 +5,7 @@ namespace Money.Decimals;
 /// <summary>
 /// Extension methods to do math on positive decimals
 /// </summary>
-public static class PositiveDecimalMath
-{
+public static class PositiveDecimalMath {
     /// <summary>
     /// Multiplies two positive decimal values together guaranteeing the result of the operation is also a positive decimal
     /// </summary>
@@ -22,6 +21,17 @@ public static class PositiveDecimalMath
             return Result<IPositiveDecimal>.Fail(new ExceptionWrapper(ex));
         }
     }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="positiveDecimal"></param>
+    /// <param name="multiplier"></param>
+    /// <exception cref="OverflowException"></exception>
+    /// <returns></returns>
+    public static IPositiveDecimal TimesOrThrow(this IPositiveDecimal positiveDecimal, IPositiveDecimal multiplier) => PositiveDecimal
+        .Create(positiveDecimal.Amount * multiplier.Amount)
+        .Data;
 
     /// <summary>
     /// Divides two positive decimal values together guaranteeing the result of the operation is also a positive decimal
@@ -40,7 +50,7 @@ public static class PositiveDecimalMath
     }
 
     /// <summary>
-    /// Adds two positive values together guaranteeing the result of the operation is also a positive decimal
+    /// Adds two positive values together guaranteeing the result of the operation is also a positive decimal and wrapping all errors and exceptions in a result type
     /// </summary>
     /// <param name="positiveDecimal"></param>
     /// <returns></returns>
@@ -54,4 +64,15 @@ public static class PositiveDecimalMath
             return Result<IPositiveDecimal>.Fail(new ExceptionWrapper(ex));
         }
     }
+
+    /// <summary>
+    /// Adds two positive decimals together or throws if the operation fails
+    /// </summary>
+    /// <param name="positiveDecimal"></param>
+    /// <param name="additive"></param>
+    /// <exception cref="OverflowException">when bounds of <see cref="decimal"/> are exceeded</exception>
+    /// <returns></returns>
+    public static IPositiveDecimal PlusOrThrow(this IPositiveDecimal positiveDecimal, IPositiveDecimal additive) => PositiveDecimal
+        .Create(positiveDecimal.Amount + additive.Amount)
+        .Data;
 }
