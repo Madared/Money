c31ba86d62ce3ad887b70d6b371ea779c7ed7e66
Dex <77392785+Madared@users.noreply.github.com>
2024-04-03 16:38:25 +0100

    Added Times, TimesOrThrow, DivideBy and corresponding tests

--- Money/Decimals/Math/NonNegativeMath.cs
+++ Money/Decimals/Math/NonNegativeMath.cs
@@ -17,4 +17,32 @@ public static class NonNegativeMath {
         decimal total = first.Amount + second.Amount;
         return DecimalFactory.CreateNonNegative(total).Data;
     }
+
+    public static Result<INonNegativeDecimal> Times(this INonNegativeDecimal first, INonNegativeDecimal second) {
+        try {
+            decimal total = first.Amount * second.Amount;
+            return DecimalFactory.CreateNonNegative(total);
+        }
+        catch (OverflowException ex) {
+            return Result<INonNegativeDecimal>.Fail(new ExceptionWrapper(ex));
+        }
+    }
+
+    public static INonNegativeDecimal TimesOrThrow(this INonNegativeDecimal first, INonNegativeDecimal second) {
+        decimal total = first.Amount * second.Amount;
+        return DecimalFactory.CreateNonNegative(total).Data;
+    }
+
+    public static Result<INonNegativeDecimal> DivideBy(this INonNegativeDecimal first, INonNegativeDecimal second) {
+        try {
+            decimal total = first.Amount / second.Amount;
+            return DecimalFactory.CreateNonNegative(total);
+        }
+        catch (OverflowException ex) {
+            return Result<INonNegativeDecimal>.Fail(new ExceptionWrapper(ex));
+        }
+        catch (DivideByZeroException ex) {
+            return Result<INonNegativeDecimal>.Fail(new ExceptionWrapper(ex));
+        }
+    }
 }
--- Tests/DecimalMathTests.cs
+++ Tests/DecimalMathTests.cs
@@ -32,4 +32,48 @@ public class DecimalMathTests {
     public void INonNegative_PlusOrThrow_Throws_On_Overflow() {
         Assert.Throws<OverflowException>(() => _maxValue.PlusOrThrow(_maxValue));
     }
+
+    [Fact]
+    public void INonNegative_Times_Returns_INonNegative_Success_Result() {
+        Result<INonNegativeDecimal> multiplied = _oneHundred.Times(_oneHundred);
+        Assert.True(multiplied.Succeeded);
+    }
+
+    [Fact]
+    public void INonNegative_Times_Returns_Failed_Result_On_Overflow() {
+        Result<INonNegativeDecimal> multiplied = _maxValue.Times(_maxValue);
+        Assert.True(multiplied.Failed);
+        Assert.True(multiplied.Error is ExceptionWrapper);
+    }
+
+    [Fact]
+    public void INonNegative_TimesOrThrow_Returns_INonNegative_Value() {
+        INonNegativeDecimal multiplied = _oneHundred.TimesOrThrow(_oneHundred);
+        decimal total = _oneHundred.Amount * _oneHundred.Amount;
+        Assert.Equal(total, multiplied.Amount);
+    }
+
+    [Fact]
+    public void INonNegative_TimesOrThrow_Throws_Overflow_Exception() {
+        Assert.Throws<OverflowException>(() => _maxValue.TimesOrThrow(_maxValue));
+    }
+
+    [Fact]
+    public void INonNegative_DivideBy_Returns_INonNegative_Success_Result() {
+        Result<INonNegativeDecimal> divided = _oneHundred.DivideBy(_oneHundred);
+        Assert.True(divided.Succeeded);
+    }
+
+    [Fact]
+    public void INonNegative_DivideBy_Returns_Failed_Result_When_Dividing_By_Zero() {
+        Result<INonNegativeDecimal> divided = _oneHundred.DivideBy(new ZeroDecimal());
+        Assert.True(divided.Failed);
+    }
+
+    [Fact]
+    public void INonNegative_DivideBy_Returns_Failed_Result_On_Overflow() {
+        INonNegativeDecimal tiny = DecimalFactory.CreateNonNegative(0.0000000000000000000001M).Data;
+        Result<INonNegativeDecimal> divided = _maxValue.DivideBy(tiny);
+        Assert.True(divided.Failed);
+    }
 }
