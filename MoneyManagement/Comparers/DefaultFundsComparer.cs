91159d10cf1534dfaba922d6a19c0151becd2ed3
Afonso <afonsomtsm@gmail.com>
2024-06-01 18:14:17 +0100

    code style changes

--- MoneyManagement/Comparers/DefaultFundsComparer.cs
+++ MoneyManagement/Comparers/DefaultFundsComparer.cs
@@ -5,23 +5,31 @@ using ResultAndOption.Results.GenericResultExtensions.Async;
 
 namespace MoneyManagement.Comparers;
 
-public sealed class DefaultFundsComparer : IFundsComparer {
+public sealed class DefaultFundsComparer : IFundsComparer
+{
     private readonly IFundsCurrencyConverter _converter;
 
-    public DefaultFundsComparer(IFundsCurrencyConverter converter) {
+    public DefaultFundsComparer(IFundsCurrencyConverter converter)
+    {
         _converter = converter;
     }
 
-    public Task<Result<FundsComparison>> Compare(IFunds first, IFunds second) {
-        if (first.Currency == second.Currency) {
-            return Task.FromResult(Result<FundsComparison>.Ok(SameCurrencyComparing(first, second)));
+    public Task<Result<FundsComparison>> Compare(IFunds first, IFunds second)
+    {
+        if (first.Currency != second.Currency)
+        {
+            return _converter
+                .Convert(second, first.Currency)
+                .MapAsync(converted => SameCurrencyComparing(first, converted));
         }
 
-        return _converter
-            .Convert(second, first.Currency)
-            .MapAsync(converted => SameCurrencyComparing(first, converted));
+        FundsComparison comparison = SameCurrencyComparing(first, second);
+        Result<FundsComparison> comparisonResult = Result<FundsComparison>.Ok(comparison);
+        return Task.FromResult(comparisonResult);
     }
-    private static FundsComparison SameCurrencyComparing(IFunds first, IFunds second) {
+
+    private static FundsComparison SameCurrencyComparing(IFunds first, IFunds second)
+    {
         if (first.Amount > second.Amount) return FundsComparison.Greater;
         if (first.Amount < second.Amount) return FundsComparison.Lesser;
         return FundsComparison.Equal;
--- MoneyManagement/Decimals/DecimalFactory.cs
+++ MoneyManagement/Decimals/DecimalFactory.cs
@@ -4,16 +4,19 @@ using ResultAndOption.Results.GenericResultExtensions;
 
 namespace MoneyManagement.Decimals;
 
-public static class DecimalFactory {
-    public static Result<INonNegativeDecimal> CreateNonNegative(decimal amount) => amount switch {
+public static class DecimalFactory
+{
+    public static Result<INonNegativeDecimal> CreateNonNegative(decimal amount) => amount switch
+    {
         0 => Result<INonNegativeDecimal>.Ok(new ZeroDecimal()),
-        > 0 => PositiveDecimal.Create(amount).Map(positive => positive as INonNegativeDecimal),
+        > 0 => PositiveDecimal.Create(amount).Map<PositiveDecimal, INonNegativeDecimal>(positive => positive),
         _ => Result<INonNegativeDecimal>.Fail(new UnknownError())
     };
 
-    public static Result<INonPositiveDecimal> CreateNonPositive(decimal amount) => amount switch {
+    public static Result<INonPositiveDecimal> CreateNonPositive(decimal amount) => amount switch
+    {
         0 => Result<INonPositiveDecimal>.Ok(new ZeroDecimal()),
-        < 0 => NegativeDecimal.Create(amount).Map(negative => negative as INonPositiveDecimal),
+        < 0 => NegativeDecimal.Create(amount).Map<NegativeDecimal, INonPositiveDecimal>(negative => negative),
         _ => Result<INonPositiveDecimal>.Fail(new UnknownError())
     };
 }
