60c6e7eadb2ad4f648e74a1e338d0e7464901830
Dex <afonsomtsm@gmail.com>
2025-08-17 03:15:38 +0100

    Removed unused string extensions

--- MoneyManagement/StringExtensions.cs
+++
@@ -1,7 +0,0 @@
-namespace MoneyManagement;
-
-public static class StringExtensions
-{
-    public static bool IsEmpty(this string str) => string.IsNullOrEmpty(str);
-    public static bool IsWhiteSpace(this string str) => string.IsNullOrWhiteSpace(str);
-}
